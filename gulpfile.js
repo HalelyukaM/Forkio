4dbccfe890306a03d7ecaf0025a5624052591ba1
Diana-Chassot <diana.chassot25@gmail.com>
2023-03-16 14:49:09 +0100

    testimonials grid

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const sass = require('gulp-sass')(require('sass'));
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-/* const cleanCSS = require('gulp-clean-css'); */
 const cssnano = require('gulp-cssnano');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
@@ -23,21 +22,18 @@ const paths = {
     src: 'src/scripts/**/*.js',
     dest: 'dist/scripts/'
   },
-  html:{
-    src:"index.html",
-    dest:"dist"
+  html: {
+    src: "index.html",
+    dest: "dist"
   },
-  img:{
-    src:"src/img/**/*.*",
+  img: {
+    src: "src/img/**/*.*",
     dest: "dist/img/"
   }
 };
-
-/* Tasks */
-
 /* css */
 function styles() {
-     return gulp.src(paths.styles.src)
+  return gulp.src(paths.styles.src)
     .pipe(order([
       'normalize.scss',
       'style.scss',
@@ -46,18 +42,16 @@ function styles() {
       '_editor.scss',
       '_testimonials.scss',
       '_pricing.scss',
-      'media.scss' 
-      ])) 
-      .pipe(concat('main.min.css'))
-      .pipe(sass().on('error', sass.logError))
-      .pipe(cssnano())
-     /*  .pipe(cleanCSS({ restructuring: false })) */
-      .pipe(autoprefixer())
-      .pipe(gulp.dest(paths.styles.dest))
-      .pipe(browserSync.stream());
-      
-  }
- /* js */
+      'media.scss'
+    ]))
+    .pipe(concat('main.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+/* js */
 function scripts() {
   return gulp.src(paths.scripts.src)
     .pipe(babel())
@@ -66,32 +60,30 @@ function scripts() {
     .pipe(gulp.dest(paths.scripts.dest))
     .pipe(browserSync.stream());
 }
-
-/*html */ 
+/*html */
 function html() {
   return gulp.src(paths.html.src)
-  .pipe(gulp.dest(paths.html.dest))
-  .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
 }
 /* image optimization */
 function img() {
   return gulp.src(paths.img.src)
-  .pipe(newer(paths.img.dest))
-  .pipe(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    imagemin.mozjpeg({quality: 75, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-    imagemin.svgo({
-      plugins: [
-        {removeViewBox: true},
-        {cleanupIDs: false}
-      ]
-    })
-  ]))
-  .pipe(gulp.dest(paths.img.dest));
+    .pipe(newer(paths.img.dest))
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest(paths.img.dest));
 }
-
- /* watch */
+/* watch */
 function watch() {
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
@@ -100,20 +92,19 @@ function watch() {
 }
 /* browsersync */
 const server = () => {
-	browserSync.init({
-		server: {
-			baseDir: "./dist/"
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
 };
 /* clean dist */
 function clearDist() {
-  return gulp.src(['dist'], {read: false, allowEmpty: true})
+  return gulp.src(['dist'], { read: false, allowEmpty: true })
     .pipe(clean());
 }
+exports.build = gulp.series(clearDist, html, styles, scripts, img);
+exports.dev = gulp.series(html, styles, scripts, img, gulp.parallel(server, watch));
 
-exports.build = gulp.series(clearDist, html, styles, scripts,  img );
-exports.dev = gulp.series(html, styles, scripts, img, gulp.parallel(server, watch)); 
-exports.clearDist =clearDist;
 
 
